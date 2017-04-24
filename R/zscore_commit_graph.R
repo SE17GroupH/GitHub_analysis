95e0bc22d1681c603e4129fc9a39e6261baec13b
Harold Chen <ychen71@ncsu.edu>
2017-04-24 14:16:13 -0400

    change R script, uploda plots for number of commits uing zscore

--- R/zscore_commit_graph.R
+++ R/zscore_commit_graph.R
@@ -1,6 +1,11 @@
 data<-read.csv('group1_commit_data.csv')
 commit<-data$commits
+norm_commit<-rnorm(commit, mean =0, sd=1)
 scale(commit, center = TRUE, scale = TRUE)
 zscore<-(commit-mean(commit)/sd(commit))
+zscore_norm<-(norm_commit-mean(norm_commit)/sd(norm_commit))
+plot(week, zscore_norm, type="o", col="blue")
+axis(side=1, at=c(1:16))
+
 plot(week,zscore, type="o", col="blue")
 axis(side=1, at=c(1:16))
