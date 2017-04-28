530bef7e438280c65072e769a6f5012961186c54
Harold Chen <ychen71@ncsu.edu>
2017-04-28 19:21:59 -0400

    update file name, upload issues created by plot

--- R/assignee.R
+++
@@ -1,9 +0,0 @@
-data<-read.csv("SE17GroupH_Zap_issues.csv")
-#assignee combination
-assignee<-data$assignee
-counts<-table(assignee)
-ordered_counts<-sort(counts)
-barplot(ordered_counts, main="Issues assigned to team members", names.arg="")
-text(x=midpts, y=-2, names(ordered_counts), cex=0.8, srt=45, xpd=TRUE)
-
-
