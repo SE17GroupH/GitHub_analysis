cf5521b9d74e1af5088c6b7e0af327b8436e2e91
Harold Chen <ychen71@ncsu.edu>
2017-04-29 21:20:35 -0400

    add weekly issue plots for group0 to group3

--- R/issues_plot.R
+++ R/issues_plot.R
@@ -11,3 +11,17 @@ created_by<-data$created_by
 counts<-table(created_by)
 ordered_counts<-sort(counts)
 barplot(ordered_counts, main="Issues created by team members")
+
+#issue open by week
+data<-read.csv("Group0_issues.csv")
+created_at<-data$created_at
+open_dates<-strftime(created_at, format="%W")
+open_counts<-table(open_dates)
+barplot(open_counts, main="Issues opened", xlab="week")
+
+
+#issue closed by week
+closed_at<-data$closed_at
+closed_dates<-strftime(closed_at, format="%W")
+closed_counts<-table(closed_dates)
+barplot(closed_counts, main="Issues closed", xlab="week")
